78c7b7e9ef540758990e8a4815fe720619cb800d
Emmanuel Isenah <emmanuelisenah@gmail.com>
2026-02-16 18:29:31 +0100

    refactor: remove effect for yargs

--- packages/huab/src/bin.ts
+++ packages/huab/src/bin.ts
@@ -1,25 +1,17 @@
-import { NodeContext, NodeRuntime, NodeTerminal } from "@effect/platform-node";
-import { Cause, Effect, Exit, Layer } from "effect";
-import { CliService } from "./cli.ts";
+#!/usr/bin/env node
+import { createCli } from "./cli.ts";
 
-const layers = Layer.mergeAll(
-  NodeContext.layer,
-  NodeTerminal.layer,
-  CliService.Default,
-);
+const cli = createCli();
 
-Effect.gen(function* () {
-  const cli = yield* CliService;
-  yield* cli.run(process.argv);
-}).pipe(
-  Effect.provide(layers),
-  NodeRuntime.runMain({
-    teardown: (exit) => {
-      // Force exit: copy-paste library spawns child processes (pbcopy/xclip/clip),
-      // which keeps Node's event loop alive even after the command completes successfully
-      const code =
-        Exit.isFailure(exit) && !Cause.isInterruptedOnly(exit.cause) ? 1 : 0;
-      process.exit(code);
-    },
-  }),
-);
+cli
+  .parseAsync()
+  .then(() => {
+    // Force exit: copy-paste library spawns child processes (pbcopy/xclip/clip),
+    // which keeps Node's event loop alive even after the command completes successfully
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
--- packages/huab/src/cli.ts
+++ packages/huab/src/cli.ts
@@ -1,33 +1,24 @@
-import { Command } from "@effect/cli";
-import { Effect } from "effect";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { NAME, VERSION } from "./constants.ts";
 
-const exampleCommand = Command.make("configure", {}, () =>
-  Effect.gen(function* () {
-    console.log(
-      "This is an example command. You can replace this with your actual command implementation.",
-    );
-  }),
-);
-
-const mainCommand = Command.make(NAME, {}, () =>
-  Effect.sync(() => {
-    console.log(
-      `${NAME} v${VERSION}. run ${NAME} --help for more information.`,
-    );
-  }),
-).pipe(Command.withSubcommands([exampleCommand]));
-
-const cliService = Effect.gen(function* () {
-  return {
-    run: (argv: string[]) =>
-      Command.run(mainCommand, {
-        name: NAME,
-        version: VERSION,
-      })(argv),
-  };
-});
-
-export class CliService extends Effect.Service<CliService>()("CliService", {
-  effect: cliService,
-}) {}
+export function createCli() {
+  return yargs(hideBin(process.argv))
+    .scriptName(NAME)
+    .version(VERSION)
+    .command(
+      "configure",
+      "Configure the application",
+      () => {},
+      () => {
+        console.log(
+          "This is an example command. You can replace this with your actual command implementation.",
+        );
+      },
+    )
+    .demandCommand(0, "")
+    .epilogue(`${NAME} v${VERSION}. Run ${NAME} --help for more information.`)
+    .help()
+    .alias("h", "help")
+    .alias("v", "version");
+}
