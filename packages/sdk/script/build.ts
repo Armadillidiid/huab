f17f3675cb612151d6a0a0e49e1098e7fd093ce1
Emmanuel Isenah <emmanuelisenah@gmail.com>
2026-02-17 18:12:42 +0100

    refactor: update sdk

--- packages/sdk/script/build.ts
+++ packages/sdk/script/build.ts
@@ -5,18 +5,18 @@ import path from "path";
 
 import { createClient } from "@hey-api/openapi-ts";
 
-const dir = new URL(".", import.meta.url).pathname;
+const dir = new URL("..", import.meta.url).pathname;
 
 // Generate OpenAPI spec
 await $`bun run gen:openapi > ${dir}/openapi.json`.cwd(
-  path.resolve(dir, "../../huab/"),
+  path.resolve(dir, "../huab"),
 );
 
 // Create TS client
 await createClient({
   input: "./openapi.json",
   output: {
-    path: "./src/gen",
+    path: "js/src/gen",
     tsConfigPath: path.join(dir, "tsconfig.json"),
     clean: true,
   },
@@ -25,13 +25,15 @@ await createClient({
       name: "@hey-api/typescript",
       exportFromIndex: false,
     },
-    // {
-    //   name: "@hey-api/sdk",
-    //   instance: "HuabClient",
-    //   exportFromIndex: false,
-    //   auth: false,
-    //   paramsStructure: "flat",
-    // },
+    {
+      name: "@hey-api/sdk",
+      instance: "HuabClient",
+      exportFromIndex: false,
+      auth: false,
+      operations: {
+        strategy: "flat",
+      },
+    },
     {
       name: "@hey-api/client-fetch",
       exportFromIndex: false,
