ded79b8467c821462803a1374045311835464175
Emmanuel Isenah <emmanuelisenah@gmail.com>
2026-02-17 17:03:17 +0100

    feat: add sdk package.json

---
+++ packages/sdk/script/build.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env bun
+
+import { $ } from "bun";
+import path from "path";
+
+import { createClient } from "@hey-api/openapi-ts";
+
+const dir = new URL(".", import.meta.url).pathname;
+
+// Generate OpenAPI spec
+await $`bun dev gen:openapi > ${dir}/openapi.json`.cwd(
+  path.resolve(dir, "../../huab/"),
+);
+
+// Create TS client
+await createClient({
+  input: "./openapi.json",
+  output: {
+    path: "./src/gen",
+    tsConfigPath: path.join(dir, "tsconfig.json"),
+    clean: true,
+  },
+  plugins: [
+    {
+      name: "@hey-api/typescript",
+      exportFromIndex: false,
+    },
+    {
+      name: "@hey-api/sdk",
+      instance: "HuabClient",
+      exportFromIndex: false,
+      auth: false,
+      paramsStructure: "flat",
+    },
+    {
+      name: "@hey-api/client-fetch",
+      exportFromIndex: false,
+      baseUrl: "http://localhost:4096",
+    },
+  ],
+});
+
+// TODO: format openapi spec
